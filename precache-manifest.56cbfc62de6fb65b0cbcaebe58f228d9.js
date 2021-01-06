ea8d6a03bd46164b335bca08c8215bff562ca7fb
Patrik Kozak <patrik9@comcast.net>
2021-01-05 23:26:59 -0500

    Updates

---
+++ precache-manifest.56cbfc62de6fb65b0cbcaebe58f228d9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2a358177fd4350982bad8ccd2b179f",
+    "url": "/PatrikKozak4.github.io/index.html"
+  },
+  {
+    "revision": "7a2ba43e7e6c43124a89",
+    "url": "/PatrikKozak4.github.io/static/css/2.3e39b39a.chunk.css"
+  },
+  {
+    "revision": "2cd166f636fd46e327ec",
+    "url": "/PatrikKozak4.github.io/static/css/main.b17f0c5e.chunk.css"
+  },
+  {
+    "revision": "7a2ba43e7e6c43124a89",
+    "url": "/PatrikKozak4.github.io/static/js/2.fb0c016f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/PatrikKozak4.github.io/static/js/2.fb0c016f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd166f636fd46e327ec",
+    "url": "/PatrikKozak4.github.io/static/js/main.2f3b665d.chunk.js"
+  },
+  {
+    "revision": "d666e6876dbb95f7ab24",
+    "url": "/PatrikKozak4.github.io/static/js/runtime-main.9b578ac6.js"
+  },
+  {
+    "revision": "24f282772d11b68e8364396ed7816b3a",
+    "url": "/PatrikKozak4.github.io/static/media/ADLogo3Dim.24f28277.glb"
+  },
+  {
+    "revision": "70c2210a1e86c3d37c964b15aca983a4",
+    "url": "/PatrikKozak4.github.io/static/media/AELogo3DLo.70c2210a.glb"
+  },
+  {
+    "revision": "353ecc6596b93f50c6ef19623a229e0b",
+    "url": "/PatrikKozak4.github.io/static/media/BridgmanScetch1.353ecc65.png"
+  },
+  {
+    "revision": "a72db8ff159eb0bff9d37403fdc48c46",
+    "url": "/PatrikKozak4.github.io/static/media/DenverScetchv6.a72db8ff.png"
+  },
+  {
+    "revision": "83dae5ae31e200ddd7dec0218cb424dd",
+    "url": "/PatrikKozak4.github.io/static/media/GrandRapidsScetchv2.83dae5ae.png"
+  },
+  {
+    "revision": "01b89545ff920d4b20599cadf51e6068",
+    "url": "/PatrikKozak4.github.io/static/media/HourNAtionArcccc.01b89545.glb"
+  },
+  {
+    "revision": "0abce74b0c7005c6da1c9c8825880444",
+    "url": "/PatrikKozak4.github.io/static/media/JaredHeadshot.0abce74b.jpg"
+  },
+  {
+    "revision": "5385a2ab8d3eee3286205a9ccc4db925",
+    "url": "/PatrikKozak4.github.io/static/media/PatrikHeadshot1.5385a2ab.jpg"
+  },
+  {
+    "revision": "479b60e057cf073c4797bebd44668429",
+    "url": "/PatrikKozak4.github.io/static/media/SwainHeadshot.479b60e0.jpg"
+  },
+  {
+    "revision": "b61df86c36aa8113de67fd7e259914c3",
+    "url": "/PatrikKozak4.github.io/static/media/TryThis.b61df86c.glb"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/PatrikKozak4.github.io/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/PatrikKozak4.github.io/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "37b0fa7af82cf0d5b23420f76a80dec4",
+    "url": "/PatrikKozak4.github.io/static/media/portraitCoop.37b0fa7a.png"
+  }
+]);
